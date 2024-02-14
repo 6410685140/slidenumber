51171644be2378733bf30ff535ef1de8b278f1f9
IOS <6410685140@student.tu.ac.th>
2024-02-14 10:44:52 +0700

    Initial Commit

---
+++ A02/A02App.swift
@@ -0,0 +1,17 @@
+//
+//  A02App.swift
+//  A02
+//
+//  Created by นายธนภัทร สาระธรรม on 14/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct A02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A02/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  A02
+//
+//  Created by นายธนภัทร สาระธรรม on 14/2/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
